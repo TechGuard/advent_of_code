b63ad5c8dcc73bebe543105ff0f967fe4f8aec30
Robin van Nunen <robin@vnunen.nl>
2018-12-04 23:58:47 +0100

    Cleanup day02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     println!("2st Answer = {}", get_answer2(&input));
 }
 
-fn get_answer1(input: &String) -> i32 {
+fn get_answer1(input: &str) -> i32 {
     let mut total_2 = 0;
     let mut total_3 = 0;
 
@@ -22,50 +22,50 @@ fn get_answer1(input: &String) -> i32 {
         }
 
         // Count 2
-        for (_, i) in &map {
-            if *i == 2 {
-                total_2 += 1;
-                break;
-            }
+        if map.values().any(|&x| x == 2) {
+            total_2 += 1;
         }
         // Count 3
-        for (_, i) in &map {
-            if *i == 3 {
-                total_3 += 1;
-                break;
-            }
+        if map.values().any(|&x| x == 3) {
+            total_3 += 1;
         }
     }
 
     total_2 * total_3
 }
 
-fn get_answer2(input: &String) -> String {
+fn get_answer2(input: &str) -> String {
     let lines: Vec<_> = input.lines().collect();
-    let mut answer = String::new();
 
-    for (offset, line1) in lines.iter().enumerate() {
-        let chars1: Vec<_> = line1.chars().collect();
-
-        'line2: for line2 in lines.iter().skip(offset + 1) {
-            let mut chars2 = line2.chars();
-            let mut diff_pos: Option<usize> = None;
-
-            for (i, c) in chars1.iter().enumerate() {
-                if chars2.next().unwrap() != *c {
-                    if diff_pos.is_some() {
-                        // more than one change
-                        continue 'line2;
-                    }
-                    diff_pos = Some(i);
-                }
-            }
-
-            if diff_pos.is_some() {
-                answer = line1.to_string();
-                answer.remove(diff_pos.unwrap());
+    for (offset, &line1) in lines.iter().enumerate() {
+        for &line2 in lines.iter().skip(offset + 1) {
+            // Find the one and only difference
+            if let Some(diff_pos) = find_one_diff(line1, line2) {
+                // Copy line1 and remove char at diff_pos
+                let mut answer = line1.to_string();
+                answer.remove(diff_pos);
+                return answer;
             }
         }
     }
-    answer
+    String::new()
+}
+
+// Returns the index of the char index that is different. None if same or more than one differences.
+fn find_one_diff(str_a: &str, str_b: &str) -> Option<usize> {
+    let mut index = 0;
+    let mut diff_pos = None;
+
+    // Compare every char
+    for (char_a, char_b) in str_a.chars().zip(str_b.chars()) {
+        if char_a != char_b {
+            // Return None if more than one difference
+            match diff_pos {
+                None => diff_pos = Some(index),
+                _ => return None,
+            }
+        }
+        index += 1;
+    }
+    diff_pos
 }
