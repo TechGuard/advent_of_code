01baa1b7d6b1b9d617c62333478806181dec2cc2
TechGuard <677174+TechGuard@users.noreply.github.com>
2025-01-05 14:52:09 +0100

    Day23 solution

--- 2018/src/day23_experimental_emergency_teleportation.rs
+++ 2018/src/day23_experimental_emergency_teleportation.rs
@@ -8,13 +8,36 @@ pos=<50,50,50>, r=200
 pos=<10,10,10>, r=5
 ";
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Nanobot {
     pos: (i32, i32, i32),
     radius: i32,
 }
 
-pub fn main(input: &str) -> (usize, String) {
+impl Nanobot {
+    fn in_range(&self, other: &Self) -> bool {
+        let diff = (
+            self.pos.0 - other.pos.0,
+            self.pos.1 - other.pos.1,
+            self.pos.2 - other.pos.2,
+        );
+        let dist = diff.0.abs() + diff.1.abs() + diff.2.abs();
+        dist <= other.radius
+    }
+
+    fn in_range_box(&self, min: &(i32, i32, i32), max: &(i32, i32, i32)) -> bool {
+        // get box closest point to sphere center by clamping
+        let x = min.0.max(self.pos.0.min(max.0));
+        let y = min.1.max(self.pos.1.min(max.1));
+        let z = min.2.max(self.pos.2.min(max.2));
+        // check if point is in range
+        let diff = (self.pos.0 - x, self.pos.1 - y, self.pos.2 - z);
+        let dist = diff.0.abs() + diff.1.abs() + diff.2.abs();
+        dist <= self.radius
+    }
+}
+
+pub fn main(input: &str) -> (usize, i32) {
     let nanobots: Vec<Nanobot> = input
         .lines()
         .filter_map(|line| {
@@ -32,19 +55,68 @@ pub fn main(input: &str) -> (usize, String) {
         })
         .collect();
 
+    // Part 1
     let largest = nanobots.iter().max_by_key(|b| b.radius).unwrap();
-    let ans1 = nanobots
-        .iter()
-        .filter(|b| {
-            let diff = (
-                b.pos.0 - largest.pos.0,
-                b.pos.1 - largest.pos.1,
-                b.pos.2 - largest.pos.2,
-            );
-            let dist = diff.0.abs() + diff.1.abs() + diff.2.abs();
-            dist <= largest.radius
-        })
-        .count();
+    let ans1 = nanobots.iter().filter(|b| b.in_range(largest)).count();
 
-    (ans1, String::from("Not implemented"))
+    // Part 2: Binary search AABB
+    let mut min = nanobots
+        .iter()
+        .map(|Nanobot { pos, radius }| (pos.0 - radius, pos.1 - radius, pos.2 - radius))
+        .min()
+        .unwrap();
+    let mut max = nanobots
+        .iter()
+        .map(|Nanobot { pos, radius }| (pos.0 + radius + 1, pos.1 + radius + 1, pos.2 + radius + 1))
+        .max()
+        .unwrap();
+    let ans2 = loop {
+        // Found solution when box size is zero (aka a point)
+        let size = (max.0 - min.0, max.1 - min.1, max.2 - min.2);
+        if size.0 == 0 && size.1 == 0 && size.2 == 0 {
+            break min.0.abs() + min.1.abs() + min.2.abs();
+        }
+
+        let mut split_min = vec![(0, 0, 0); 2];
+        let mut split_max = vec![(0, 0, 0); 2];
+
+        // Split on largest axis
+        if size.0 >= size.1 && size.0 >= size.2 {
+            let split = (min.0 + max.0) / 2;
+            split_min[0] = (min.0, min.1, min.2);
+            split_min[1] = (split + 1, min.1, min.2);
+            split_max[0] = (split, max.1, max.2);
+            split_max[1] = (max.0, max.1, max.2);
+        } else if size.1 >= size.0 && size.1 >= size.2 {
+            let split = (min.1 + max.1) / 2;
+            split_min[0] = (min.0, min.1, min.2);
+            split_min[1] = (min.0, split + 1, min.2);
+            split_max[0] = (max.0, split, max.2);
+            split_max[1] = (max.0, max.1, max.2);
+        } else {
+            let split = (min.2 + max.2) / 2;
+            split_min[0] = (min.0, min.1, min.2);
+            split_min[1] = (min.0, min.1, split + 1);
+            split_max[0] = (max.0, max.1, split);
+            split_max[1] = (max.0, max.1, max.2);
+        }
+
+        // For each section find all overlapping nanobots
+        let (next_index, _) = (0..2)
+            .map(|i| {
+                nanobots
+                    .iter()
+                    .filter(|b| b.in_range_box(&split_min[i], &split_max[i]))
+                    .count()
+            })
+            .enumerate()
+            .rev()
+            .max_by_key(|(_, b)| *b)
+            .unwrap();
+
+        // Continue with largest section (if there's a tie prefer index 0)
+        min = split_min[next_index];
+        max = split_max[next_index];
+    };
+    (ans1, ans2)
 }
