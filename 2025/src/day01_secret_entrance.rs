e14ac48791ee285a5fbf790e88d8abe0684297f3
TechGuard <677174+TechGuard@users.noreply.github.com>
2025-12-01 12:13:52 +0100

    Part 2

--- 2025/src/day01_secret_entrance.rs
+++ 2025/src/day01_secret_entrance.rs
@@ -1,5 +1,4 @@
 use anyhow::*;
-use itertools::Itertools;
 
 pub static DAY: u32 = 01;
 pub static EXAMPLE_INPUT: &str = "\
@@ -15,9 +14,10 @@ R14
 L82
 ";
 
-pub fn main(input: &str) -> Result<(String, String)> {
+pub fn main(input: &str) -> Result<(i32, i32)> {
     let mut dial = 50;
     let mut result1 = 0;
+    let mut result2 = 0;
 
     for line in input.lines() {
         let mut chars = line.chars();
@@ -28,13 +28,23 @@ pub fn main(input: &str) -> Result<(String, String)> {
         };
         let amount: i32 = chars.as_str().parse()?;
 
+        let begin_at_zero = dial == 0;
         dial += dir * amount;
-        dial %= 100;
 
+        if dial <= 0 {
+            result2 += dial.abs() / 100;
+            if !begin_at_zero {
+                result2 += 1;
+            }
+        } else if dial > 99 {
+            result2 += dial / 100;
+        }
+
+        dial = dial.rem_euclid(100);
         if dial == 0 {
             result1 += 1;
         }
     }
 
-    Ok((result1.to_string(), "Not implemented".into()))
+    Ok((result1, result2))
 }
